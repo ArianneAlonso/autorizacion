3313c1aabb4ac86847ec53ac88f60af49e06565e
ArianneAlonso <ariannealonso8@gmail.com>
2024-09-13 16:30:20 -0300

    update controllers

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -8,3 +8,28 @@ export const getAllTodosCtrl = (req, res) => {
 
   res.json({ todos });
 };
+
+export const crearTodoCtrl = (req, res) => {
+  const {title, completed} = req.body;
+
+  if (!title || completed === undefined) {
+    return res.status(400).json({ message: "se necesita un título y asiganr si esta completado" });
+}
+const nuevoTodo = {
+  id: database.todos.length + 1,
+  title,
+  completed,
+  owner: req.user.id,
+};
+
+database.todos.push(nuevoTodo);
+res.status(201).json({ message: "se ha creado exitosamente", todo: nuevoTodo});
+}; 
+
+export const updTodoCtrl = (req, res)=>{
+
+};
+
+export const dltTodoCtrl = (req, res)=>{
+
+};
