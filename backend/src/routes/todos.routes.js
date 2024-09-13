e5e2c80a492cb814d1b52d7dcc4df35e0fea480c
ArianneAlonso <ariannealonso8@gmail.com>
2024-09-13 16:40:52 -0300

    se importaron las rutas del backend

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import { getAllTodosCtrl, crearTodoCtrl, updTodoCtrl, dltTodoCtrl} from "../controllers/todos.controllers.js";
 import validartJwt from "../middlewares/validar-jwt.js"
 
 const todosRouter = Router();
 
 todosRouter.get("/",validartJwt, getAllTodosCtrl);
-todosRouter.post("/");
-todosRouter.post("/");
+todosRouter.post("/", validartJwt, crearTodoCtrl);
+todosRouter.put("/:id", validartJwt, updTodoCtrl );
+todosRouter.delete("/:id", validartJwt, dltTodoCtrl)
 
 export { todosRouter };
