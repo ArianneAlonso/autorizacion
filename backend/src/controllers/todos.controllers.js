0f5dec92eb34c5c92dc5712cf8ebcc4d5b2a472a
ArianneAlonso <ariannealonso8@gmail.com>
2024-09-13 15:50:22 -0300

    update

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,7 +1,10 @@
 import { database } from "../db/database.js";
 
 export const getAllTodosCtrl = (req, res) => {
-  const todos = database.todos;
+  
+  console.log(req.user)
+
+  const todos = database.todos.filter((todo)=> todo.owner === req.user.id)
 
   res.json({ todos });
 };
--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { getAllTodosCtrl } from "../controllers/todos.controllers.js";
+import validartJwt from "../middlewares/validar-jwt.js"
 
 const todosRouter = Router();
 
-todosRouter.get("/", getAllTodosCtrl);
+todosRouter.get("/",validartJwt, getAllTodosCtrl);
+todosRouter.post("/");
+todosRouter.post("/");
 
 export { todosRouter };
